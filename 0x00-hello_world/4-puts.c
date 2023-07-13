bf83d09ab6cf448cff2d8c950f8912ba2d1de60a
Rop Kelvin <kropchir@gmail.com>
2023-07-13 11:58:01 +0000

    Print using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* C program to print using the 'puts' function */
+
+#include<stdio.h>
+int main(void)	/* Main block */
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
