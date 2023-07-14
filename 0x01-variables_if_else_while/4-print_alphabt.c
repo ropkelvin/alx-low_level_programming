6ca63fbc7c6f7911db393e8a9f30e30b9bc7879d
Rop Kelvin <kropchir@gmail.com>
2023-07-14 10:28:26 +0000

    Prints the alphabet in lowercase except some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	{
+		if ((alp == 'q' || alp == 'e') != 1)
+		{
+			putchar(alp);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
