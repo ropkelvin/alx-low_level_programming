314523864a91a722f871a3c78820b7d9dec98a73
Rop Kelvin <kropchir@gmail.com>
2023-07-18 11:52:57 +0000

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
+int main(void)
+{
+	char *c = "_putchar\n";
+
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
+	}
+	return (0);
+}
