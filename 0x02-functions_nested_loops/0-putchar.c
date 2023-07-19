dfcfd830086155bfa4300e6ea7dddc857e165f7e
Rop Kelvin <kropchir@gmail.com>
2023-07-19 12:21:47 +0000

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,14 @@
 
 int main(void)
 {
-	char *c = "_putchar\n";
-
-	while (*c)
-	{
-		_putchar(*c);
-		c++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
